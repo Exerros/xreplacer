b13afdb084781329f83b85709e630d15622a16ca
Exerros <dvartemenkowork@gmail.com>
2021-06-16 13:39:20 +0300

    Update 16.06.2021

--- modules/logger/module_info.cmake
+++ modules/logger/module_info.cmake
@@ -1,1 +1,11 @@
+################################################################################
+#
+#
  
+set(MODULE_NAME "xrep_logger")
+set(MODULE_INCLUDE "include/")
+set(MODULE_SRC "src/")
+set(MODULE_PROPERTIES LINKER_LANGUAGE CXX CXX_STANDARD 17)
+set(MODULE_COMPILE_FEATURES cxx_std_17)
+set(MODULE_COMPILE_OPTIONS -Wall -Wextra -Wpedantic)
+set(MODULE_LINK_WITH xrep pugixml)
